0e43ab94ce92c4b85842890ea2d383e8e09b180f
digitaldevine <mishraprakhar2580@gmail.com>
2026-07-01 21:17:18 +0530

    Server.js

--- Server.js
+++ Server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(cors({
     origin: [
     "http://localhost:3000",
-    "https://quiz-whiz-chi.vercel.app"
+    "https://quiz-whiz-umber.vercel.app"
   ],
   credentials: true
 }));
