f6ab3c49c23140112f0cea99f95495abb75e4670
digitaldevine <mishraprakhar2580@gmail.com>
2026-07-01 20:25:43 +0530

    Routes/auth.js

--- Routes/auth.js
+++ Routes/auth.js
@@ -10,89 +10,6 @@ const User = require("../Models/user");
 
 const auth = require("../middleware/auth");
 
-
-
-router.post("/signup", async (req, res) => {
-
-    try {
-
-        const {
-
-            username,
-            email,
-            password
-
-        } = req.body;
-
-        const exist = await User.findOne({
-
-            email
-
-        });
-
-        if (exist) {
-
-            return res.json({
-
-                success: false,
-                message: "Email already exists"
-
-            });
-
-        }
-
-        const hash = await bcrypt.hash(password, 10);
-
-        const user = await User.create({
-
-            username,
-            email,
-            password: hash
-
-        });
-
-        const token = jwt.sign(
-
-            { id: user._id },
-
-            process.env.JWT_SECRET,
-
-            {
-
-                expiresIn: "7d"
-
-            }
-
-        );
-
-        res.json({
-
-            success: true,
-
-            token,
-
-            user
-
-        });
-
-    }
-
-    catch (err) {
-
-        res.status(500).json({
-
-            success: false,
-
-            message: err.message
-
-        });
-
-    }
-
-});
-
-
-
 router.post("/signin", async (req, res) => {
 
     try {
@@ -181,6 +98,91 @@ router.post("/signin", async (req, res) => {
     }
 
 });
+
+
+
+router.post("/signup", async (req, res) => {
+
+    try {
+
+        const {
+
+            username,
+            email,
+            password
+
+        } = req.body;
+
+        const exist = await User.findOne({
+
+            email
+
+        });
+
+        if (exist) {
+
+            return res.json({
+
+                success: false,
+                message: "Email already exists"
+
+            });
+
+        }
+
+        const hash = await bcrypt.hash(password, 10);
+
+        const user = await User.create({
+
+            username,
+            email,
+            password: hash
+
+        });
+
+        const token = jwt.sign(
+
+            { id: user._id },
+
+            process.env.JWT_SECRET,
+
+            {
+
+                expiresIn: "7d"
+
+            }
+
+        );
+
+        res.json({
+
+            success: true,
+
+            token,
+
+            user
+
+        });
+
+    }
+
+    catch (err) {
+
+        res.status(500).json({
+
+            success: false,
+
+            message: err.message
+
+        });
+
+    }
+
+});
+
+
+
+
 
 
 
